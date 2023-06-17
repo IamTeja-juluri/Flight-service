16787e88797842884ed4fac0780613279fee200a
Teja Juluri <julurisaiteja6789@gmail.com>
2023-06-17 19:08:01 +0530

    Initial setup with one crud api

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const apiRoutes=require('./routes')
 
 const app=express();
 
-app.use('/api',apiRoutes)
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.use('/api',apiRoutes);
 
 
 app.listen(ServerConfig.PORT,()=>{
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,15 @@
 const express= require('express');
 
-const { InfoController }=require('../../controllers')
+const { InfoController }=require('../../controllers');
+
+const airplaneRoutes=require('./airplane-routes');
 
 const router=express.Router();
 
-router.get('/info',InfoController.info)
 
-module.exports=router
+router.use('/airplanes',airplaneRoutes);
+
+router.get('/info',InfoController.info);
+
+
+module.exports=router;
