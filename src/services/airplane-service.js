bfa6cf63c9ba36f713b9699a20b8a79bc9748f4d
Teja Juluri <julurisaiteja6789@gmail.com>
2023-07-07 19:25:52 +0530

    added get airplane

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -36,9 +36,20 @@ async function getAirplanes(){
     }
 }
 
-
+async function getAirplane(id){
+    try{
+        const airplane=await airplaneRepository.get(id);
+        return airplane;
+    }
+    catch(error){
+        if(error.statusCode ===  StatusCodes.NOT_FOUND){
+            throw new AppError("The Airplane you requested is not present",error.statusCode)
+        }
+        throw new AppError('Cannot fetch data of an airplane',StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
 
 
 module.exports={
-    createAirplane,getAirplanes
+    createAirplane,getAirplanes,getAirplane
 }
