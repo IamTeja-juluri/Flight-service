6f34f37c57636bbd6ee9015ae37cb95a40b27d42
Teja Juluri <julurisaiteja6789@gmail.com>
2023-07-31 22:34:44 +0530

    added proxy compliant

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,5 +1,6 @@
 const {StatusCodes}=require('http-status-codes')
 const info = (req,res)=>{
+    console.log("Req recieved")
     return res.status(StatusCodes.OK).json({
         success:true,
         message:'API is live',
